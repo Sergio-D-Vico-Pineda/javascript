7e691bd68a61eedd70ba822afb5c3b7c63cd1fca
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-10-04 20:39:05 +0200

    css, my class, new btn to change color

--- script/main.js
+++ script/main.js
@@ -8,7 +8,7 @@ function buttonClicked() {
 function onload() {
     count = document.getElementById("count").textContent;
     console.log("DOM fully loaded and parsed");
-    
+
 
     let btn = document.getElementById("btn");
     /* btn.addEventListener("click", buttonClicked); */
@@ -16,6 +16,20 @@ function onload() {
 
     let btn1 = document.getElementById("btn1");
     btn1.addEventListener("click", buttonClicked);
+
+    let Ps = document.getElementsByClassName("scarpy");
+    for (const p1 of Ps) {
+        console.log(p1.textContent);
+        p1.style.color = "red";
+    }
+    Ps[2].classList.add("negrita");
+    Ps[3].classList.remove("sergio");
+
+    let btn2 = document.getElementById("btn2");
+    btn2.addEventListener("click", function () {
+        Ps[0].style.color == "blue" ? Ps[0].style.color = "red" :
+            Ps[0].style.color = "blue";
+    });
 }
 
 document.addEventListener("DOMContentLoaded", onload);
