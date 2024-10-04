f1b00a9a509fffc492f5b12a44f437f23f13b0b8
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-10-04 20:16:05 +0200

    counter in html

--- script/main.js
+++ script/main.js
@@ -1,11 +1,14 @@
-let count = 0;
+let count;
 function buttonClicked() {
     count++;
     console.log("Button clicked:" + count);
+    document.getElementById("count").textContent = count;
 }
 
 function onload() {
+    count = document.getElementById("count").textContent;
     console.log("DOM fully loaded and parsed");
+    
 
     let btn = document.getElementById("btn");
     /* btn.addEventListener("click", buttonClicked); */
