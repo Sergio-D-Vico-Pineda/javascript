2f73e3c54986976378bc28bc21889a6751b02dc6
Scarpy19_ <nootravez19@gmail.com>
2024-10-17 12:20:47 +0200

    imports and first task

--- To Doist/script.js
+++ To Doist/script.js
@@ -1,6 +1,19 @@
+"use strict"
+
+import * as gt from "./js/gestionTasks.js";
+import * as gw from "./js/gestionWeb.js";
+
+// Prevent confirm from resend
 window.history.replaceState(null, null, window.location.href);
 
-function toLocalISOString(date)
+gt.addTask(new gt.NewTask("Test", true, "2021-12-31 05:10", ["tag1", "tag2", "tag3"]));
+
+for (let task of gt.taskList())
+{
+    gw.showTask("toDoList", task);
+}
+
+/* function toLocalISOString(date)
 {
     const localDate = new Date(date - date.getTimezoneOffset() * 60000); //offset in milliseconds. Credit https://stackoverflow.com/questions/10830357/javascript-toisostring-ignores-timezone-offset
     localDate.setSeconds(null);
@@ -13,4 +26,4 @@ document.getElementById("date").value = toLocalISOString(new Date());
 document.getElementById("tForm").addEventListener("submit", function(e)
 {
     e.preventDefault(); // Prevents default form submission
-})
+}) */
