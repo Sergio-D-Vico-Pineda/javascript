d3fa8c1a0f108506eac09c6974bb7b6c4d20d8ec
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-10-18 20:52:00 +0200

    formating things

--- To Doist/script.js
+++ To Doist/script.js
@@ -6,24 +6,6 @@ import * as gw from "./js/gestionWeb.js";
 // Prevent confirm from resend
 window.history.replaceState(null, null, window.location.href);
 
-gt.addTask(new gt.NewTask("Test", true, "2021-12-31 05:10", ["tag1", "tag2", "tag3"]));
+gt.addTask(new gt.NewTask("Test", true, "2021-12-31 05:10", ["test", " tag1", "tag2"]));
 
-for (let task of gt.taskList())
-{
-    gw.showTask("toDoList", task);
-}
-
-/* function toLocalISOString(date)
-{
-    const localDate = new Date(date - date.getTimezoneOffset() * 60000); //offset in milliseconds. Credit https://stackoverflow.com/questions/10830357/javascript-toisostring-ignores-timezone-offset
-    localDate.setSeconds(null);
-    localDate.setMilliseconds(null);
-    return localDate.toISOString().slice(0, -1);
-}
-
-document.getElementById("date").value = toLocalISOString(new Date());
-
-document.getElementById("tForm").addEventListener("submit", function(e)
-{
-    e.preventDefault(); // Prevents default form submission
-}) */
+gw.repaint();
