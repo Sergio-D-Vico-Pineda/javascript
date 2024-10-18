0f5c40a68787f697faf6fa6ad9326c3464b98810
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-10-18 20:52:47 +0200

    get task and update all

--- To Doist/js/gestionTasks.js
+++ To Doist/js/gestionTasks.js
@@ -1,6 +1,7 @@
 let tasks = [],
     taskId = 0;
 
+// Crear una nueva tarea
 function NewTask(name, active, date, ...tags)
 {
     this.name = name;
@@ -17,29 +18,47 @@ function NewTask(name, active, date, ...tags)
     {
         this.date = Date.parse(date);
     }
+
+    this.updateName = (newName) => this.name = newName;
+
+    this.updateActive = (newActive) => this.active = newActive;
+
+    // this.updateDate = (newDate) => this.date = Date.parse(newDate);
+
+    this.updateTags = (newTags) => this.tags = newTags;
 }
 
+// Devolver el array de tareas
 function taskList()
 {
     return tasks;
 }
 
+function getTask(id)
+{
+    return tasks.find(task => task.id === id);
+}
+
+// Añadir una nueva tarea en el array
 function addTask(task)
 {
     task.id = taskId++;
     tasks.push(task);
 }
 
+// Borrar una tarea del array
 function deleteTask(id)
 {
     tasks = tasks.filter(task => task.id !== id);
 }
 
+// Contar el número de tareas
 function countTasks()
 {
     return tasks.length;
 }
 
+// Convertir una fecha local a UTC
 function toLocalISOString(date)
 {
     const localDate = new Date(date - date.getTimezoneOffset() * 60000); //offset in milliseconds. Credit https://stackoverflow.com/questions/10830357/javascript-toisostring-ignores-timezone-offset
@@ -48,7 +67,7 @@ function toLocalISOString(date)
     return localDate.toISOString().slice(0, -1);
 }
 
-
+// Convertir una fecha UTC a local
 function fromLocalISOString(dateString)
 {
     const date = new Date(dateString);
@@ -71,5 +90,6 @@ export
     deleteTask,
     countTasks,
     toLocalISOString,
-    fromLocalISOString
+    fromLocalISOString,
+    getTask
 };
